9b9a22d6c0bdfceef57ece2c0ba630bbc061b11a
Austin <austinm9506@gmail.com>
2021-08-08 21:55:47 -0400

    Added a component function for getting all tasks to clean up App.js

---
+++ client/src/components/AllTasks.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function AllTasks () {
+    const [taskList, setTasks] = useState([])
+  
+    useEffect(() => {
+      axios.get("/api/tasks/").then((res) => {
+        setTasks(res.data)
+        console.log(res.data)
+      })
+    }, [])
+  
+  
+    return (
+      <div className="App">
+        <div> {taskList.map((val, key) => {
+          return <div>
+            <h1>
+            {val.title}
+            </h1>
+            <h2>
+              {val.created_by}
+            </h2>
+            <o>
+              {val.description}
+            </o>
+          </div>
+        })} </div>
+      </div>
+    );
+}
+
+export default AllTasks;
