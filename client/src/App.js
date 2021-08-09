c722d8417f60131e8382f5d7b96af3ed0f91e799
Austin <austinm9506@gmail.com>
2021-08-08 21:30:03 -0400

    Created a simple react hook to display all tasks from a ToDo List

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 
 function App() {
+
+  const [taskList, setTasks] = useState([])
+  
+  useEffect(() => {
+    axios.get("/api/tasks/").then((res) => {
+      setTasks(res.data)
+      console.log(res.data)
+    })
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div> {taskList.map((val, key) => {
+        return <div>
+          <h1>
+          {val.title}
+          </h1>
+          <h2>
+            {val.created_by}
+          </h2>
+          <o>
+            {val.description}
+          </o>
+        </div>
+      })} </div>
     </div>
   );
 }
