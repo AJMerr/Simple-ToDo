8f6390e8dcbd2d09e42381f03dbab7b516d345d3
Austin <austinm9506@gmail.com>
2021-08-09 00:19:58 -0400

    Created a component function for adding a task and verified functionality.

---
+++ client/src/components/AddTask.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function AddTask () {
+
+    //Setting state
+    const initialTaskState = {
+        _id: '',
+        title: "",
+        created_by: "",
+        description: "",
+        __v: 9
+    }
+
+    const [task, setTask] = useState(initialTaskState)
+
+    const changeHandler = e => {
+        const { name, value } = e.target
+        setTask({...task, [name]: value})
+    }
+
+    const createTask = (e) => {
+        e.preventDefault()
+
+        var data = {
+            title: task.title,
+            created_by: task.created_by,
+            description: task.description
+        }
+
+        axios.post("/api/tasks", data).then((res) => {
+            setTask({
+                _id: res.data._id,
+                title: res.data.title,
+                created_by: res.data.created_by,
+                description: res.data.description,
+                __v: res.data.__v
+
+            })
+        })
+    }
+
+    return (
+        <div>
+            <form>
+                <label>New Title: </label>
+                <input
+                type="text"
+                id="title"
+                value={task.title}
+                onChange={changeHandler}
+                name="title"
+                />
+                <label>Created by: </label>
+                <input
+                type="text"
+                id="created_by"
+                value={task.created_by}
+                onChange={changeHandler}
+                name="created_by"
+                />
+                <label>Description: </label>
+                <input
+                type="text"
+                id="description"
+                value={task.description}
+                onChange={changeHandler}
+                name="description"
+                />
+                <button onClick={createTask}>Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default AddTask;
