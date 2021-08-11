82d4071226c5e644e2eba2e74db1b47a75b2acc3
Austin <austinm9506@gmail.com>
2021-08-11 01:56:57 -0400

    Added basic UI layout using Material-UI.

--- client/src/components/AllTasks.jsx
+++ client/src/components/AllTasks.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Typography, AppBar, Card, CardActions, CardContent, CssBaseline, Grid, Toolbar, Container, Button } from "@material-ui/core"
+import ListIcon from '@material-ui/icons/List';
 
 function AllTasks () {
 
@@ -16,21 +18,57 @@ function AllTasks () {
   
   
     return (
-      <div>
+      <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <ListIcon />
+          <Typography>Simple ToDo</Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div>
+          <Container maxWidth="sm">
+            <Typography variant="h1" align="center" color="textPrimary" gutterBottom>All Tasks</Typography>
+            <Typography variant="h5" align="center" color="textSecondary" paragraph>
+              This is a simple ToDo webapp built using the MERN Stack and my first test utilizing React Hooks
+            </Typography>
+            <div>
+              <Grid container spacing="2" justify="center">
+                <Grid item>
+                  <Button variant="contained" color="primary" href="http://localhost:3000/create">
+                    Create a task
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+        </Container>
+        </div>
+        <Container maxWidth="md">
         <div> {taskList.map((val, key) => {
           return <div>
-            <h1>
-            {val.title}
-            </h1>
-            <h2>
-              {val.created_by}
-            </h2>
-            <o>
-              {val.description}
-            </o>
+          <Grid container spacing="4">
+            <Grid item>
+              <Card>
+                <CardContent>
+                  <Typography variant="h5" color="textPrimary" gutterBottom>
+                    {val.title}
+                  </Typography>
+                  <Typography variant="h6" color="textSecondary">
+                    {val.created_by}
+                  </Typography>
+                  <Typography variant="h6" color="textPrimary" paragraph>
+                    {val.description}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          </Grid>
           </div>
         })} </div>
-      </div>
+        </Container>
+      </main>
+      </>
     );
 }
 
