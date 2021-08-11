3ec68259c042c8b6c39ec4cb82c0a7c360719fd0
Austin <austinm9506@gmail.com>
2021-08-10 23:39:09 -0400

    Added a routing for editing a single task.

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import AllTasks from './components/AllTasks';
 import AddTask from './components/AddTask';
 import SingleTask from './components/SingleTask';
+import EditTask from './components/EditTask';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 
@@ -19,6 +20,9 @@ function App() {
           </Route>
           <Route exact path="/task/:id">
             <SingleTask />
+          </Route>
+          <Route exact path="/task/edit/:id">
+            <EditTask />
           </Route>
         </Switch>
       </div>
