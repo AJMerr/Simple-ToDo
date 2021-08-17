1af3ece6275ed8e64a227c7de641e7130501988b
Austin <austinm9506@gmail.com>
2021-08-16 21:26:37 -0400

    Created basic styling for editing a task.

--- client/src/components/EditTask.jsx
+++ client/src/components/EditTask.jsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams } from "react-router-dom"
+import { Typography, AppBar, CssBaseline, Grid, Toolbar, Container, Button, InputLabel } from "@material-ui/core"
+import ListIcon from '@material-ui/icons/List';
+import Input from '@material-ui/core/Input';
+import { Link as RouterLink } from "react-router-dom"
 
 function EditTask (props) {
     //Setting state
@@ -49,44 +53,61 @@ function EditTask (props) {
     }
     
     return(
-        <div>
-            <h1>
-                {singleTask.title}
-            </h1>
-            <form>
-                <label>Title: </label>
-                <input
-                tpye="text"
-                id="tite"
-                name="title"
-                value={singleTask.title}
-                onChange={changeHandler}
-                />
-
-                <label>Created by: </label>
-                <input
-                tpye="text"
-                id="created_by"
-                name="created_by"
-                value={singleTask.created_by}
-                onChange={changeHandler}
-                />
-
-                <label>Title</label>
-                <input
-                tpye="text"
-                id="description"
-                name="description"
-                value={singleTask.description}
-                onChange={changeHandler}
-                />
-            </form>
-            <button
-            type="submit"
-            onClick={editTask}>
-                Edit
-            </button>
-        </div>
+        <>
+        <CssBaseline />
+        <AppBar position="relative">
+          <Toolbar>
+            <ListIcon  />
+            <Typography>Simple ToDo</Typography>
+          </Toolbar>
+        </AppBar>
+        <main>
+            <div>
+                <Container maxWidth="sm">
+                    <Typography variant="h3" align="center" color="textPrimary" gutterBottom>Your Task</Typography>
+                    <Typography variant="h6" align="center" color="textSecondary" paragraph>
+                    This page allows you to edit your task. Once you hit submit, return home!
+                    </Typography>
+                </Container>
+            </div>
+            <div>
+                <Container maxWidth="md">
+                <form>
+                    <Grid container spacing={4} justifyContent="center">
+                        <Grid item>
+                            <InputLabel>New Title: </InputLabel>
+                            <Input
+                             type="text"
+                            id="title"
+                            value={singleTask.title}
+                            onChange={changeHandler}
+                            name="title"
+                            />
+                            <InputLabel>Created by: </InputLabel>
+                            <Input
+                            type="text"
+                            id="created_by"
+                            value={singleTask.created_by}
+                            onChange={changeHandler}
+                            name="created_by"
+                            />
+                            <InputLabel>Description: </InputLabel>
+                            <Input
+                            type="text"
+                            id="description"
+                            value={singleTask.description}
+                            onChange={changeHandler}
+                            name="description"
+                            />
+                            <Button color="primary" onClick={editTask}>Submit</Button>
+                            <Button color="primary" component={RouterLink} to="/">Home</Button>
+                        </Grid>
+                    </Grid>
+                </form>
+                </Container>
+            </div>
+        </main>
+        </>
     )
 }
 
